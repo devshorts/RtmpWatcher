fd3f8757021e3c7e6943d498c94c5729b43a5b2d
khiemtong <tong.khiem@gmail.com>
2012-10-24 18:48:23 -0400

    Query and select network interfaces in managed, pass selected device to unmanaged for use with pcap.

--- RtmpWatcher/RawSocketGrabber.cpp
+++ RtmpWatcher/RawSocketGrabber.cpp
@@ -9,7 +9,8 @@
 
 #pragma comment( lib, "ws2_32.lib" ) // linker must use this lib for sockets
 
-RawSocketGrabber::RawSocketGrabber(int targetPort){
+RawSocketGrabber::RawSocketGrabber(int deviceIndex, int targetPort){
+    _targetDevice = deviceIndex;
 	_targetPort = targetPort;
 	isRunning = false;
 }
@@ -122,32 +123,7 @@ pcap_t * RawSocketGrabber::InitSocket(){
 		exit(1);
 	}
 
-	/* Print the list */
-	for(d=alldevs; d; d=d->next)
-	{
-		printf("%d. %s", ++i, d->name);
-		if (d->description)
-			printf(" (%s)\n", d->description);
-		else
-			printf(" (No description available)\n");
-	}
-
-	if(i==0)
-	{
-		printf("\nNo interfaces found! Make sure WinPcap is installed.\n");
-		return NULL;
-	}
-
-	printf("Enter the interface number (1-%d):",i);
-	scanf_s("%d", &inum);
-
-	if(inum < 1 || inum > i)
-	{
-		printf("\nInterface number out of range.\n");
-		/* Free the device list */
-		pcap_freealldevs(alldevs);
-		return NULL;
-	}
+    inum = _targetDevice;
 
 	/* Jump to the selected adapter */
 	for(d=alldevs, i=0; i< inum-1 ;d=d->next, i++);
