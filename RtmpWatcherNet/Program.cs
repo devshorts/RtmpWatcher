69545678e57a9843fa9f8904b181f384bdd4fa28
anton <akropp@gmail.com>
2012-10-25 15:01:26 -0400

    starting to remove excess interop project

--- RtmpWatcherNet/Program.cs
+++ RtmpWatcherNet/Program.cs
@@ -28,73 +28,10 @@ namespace RtmpWatcherNet
         {
             var watcher = new RtmpWatcherInterop();
 
-            var thread = new Thread(() => watcher.Start(deviceChoice, 21935, OnPacketFound));
+            var thread = new Thread(() => watcher.Start(deviceChoice, 21935, PacketHandler.OnPacketFound));
 
             thread.Start();
             thread.Join();
-        }
-
-        private static void OnPacketFound(RtmpPacketInterop obj)
-        {
-            using (var newStream = new MemoryStream(obj.GetBytes()))
-            {
-                var streamCopy = PruneStreamChunkDelimiters(newStream, obj);
-
-                Console.WriteLine("Found packet {2} from {0} length {1}", obj.GetSourceIP(), obj.GetLength(),
-                                  obj.GetRtmpPacketType());
-
-
-                var decoder = AMFSerializerUtil<object>.DeserializeAmf(streamCopy);
-
-                
-                Console.WriteLine();
-            }
-        }
-
-        private static MemoryStream PruneStreamChunkDelimiters(MemoryStream newStream, RtmpPacketInterop obj)
-        {
-            var streamCopy = new MemoryStream(obj.GetLength());
-            var count = 1;
-
-            for (int i = 0; i < 9; i++)
-            {
-                byte b = Convert.ToByte(newStream.ReadByte());
-                streamCopy.WriteByte(b);
-            }
-
-            while (newStream.Position != obj.GetLength())
-            {
-                byte b = Convert.ToByte(newStream.ReadByte());
-
-                if (count % 128 == 0 && b == 0xc3)
-                {
-                }
-                else
-                {
-                    count++;
-                    streamCopy.WriteByte(b);
-                }
-            }
-            
-            streamCopy.Seek(0, SeekOrigin.Begin);
-            var firstByte = streamCopy.ReadByte();
-            bool useLongHeader = firstByte == 0;
-            int objectId;
-            if (!useLongHeader)
-            {
-                objectId = firstByte & 0x0F;
-            }
-
-            if (useLongHeader)
-            {
-                streamCopy.Seek(12, SeekOrigin.Begin);
-            }
-            else
-            {
-                streamCopy.Seek(8, SeekOrigin.Begin);
-            }
-
-            return streamCopy;
         }
 
         private static int GetNetworkInterfaces()
