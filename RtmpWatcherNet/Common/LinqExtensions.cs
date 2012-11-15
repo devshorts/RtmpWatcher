0a556dc911a5476a16b9c9694648a740b82f539e
anton <akropp@gmail.com>
2012-11-15 14:09:11 -0500

    adding extensions and cleaning up utils

---
+++ RtmpWatcherNet/Common/LinqExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace RtmpWatcherNet.Common
+{
+    public static class LinqExtensions
+    {
+        public static string FoldToDelimitedList<TSource>(this IEnumerable<TSource> source, Func<TSource, string> formatItemFunc, string delimiter)
+        {
+            var s = source
+                .Aggregate(new StringBuilder(), (acc, item) => acc.AppendFormat("{0}{1}", formatItemFunc(item), delimiter))
+                .ToString();
+            return s.EndsWith(delimiter)
+                ? s.Substring(0, s.Length - delimiter.Length)
+                : s;
+        }
+    }
+}
---
+++ RtmpWatcherNet/Parser/AmfContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RtmpWatcherNet.Parser
+{
+    public class AmfContainer
+    {
+        public String Name { get; set; }
+        public Object Value { get; set; }
+        public List<AmfContainer> Values { get; set; }
+    }
+}
